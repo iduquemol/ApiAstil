b3a00423a5c52896d5f8dc7c3207fcad7b09357e
jhonn24merchan-debug <jhonn24merchan@gmail.com>
2026-09-04 17:19:55 -0500

    Add Factura1 API integration with token authentication and XML submission (#3)

---
+++ ApiAstil/Models/Factura1SendRequest.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ApiAstil.Models
+{
+    public class Factura1SendRequest
+    {
+        [JsonPropertyName("sucursal")]
+        public string Sucursal { get; set; } = string.Empty;
+
+        [JsonPropertyName("base64doc")]
+        public string Base64doc { get; set; } = string.Empty;
+    }
+}
